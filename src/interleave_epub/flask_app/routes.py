ba9e870c11d430e529a4a72bf4f40aec3e73ec51
Pitrified <nobilipietro@gmail.com>
2022-06-19 12:55:41 +0200

    Update dependencies and fix basic sample

--- src/interleave_epub/flask_app/routes.py
+++ src/interleave_epub/flask_app/routes.py
@@ -9,6 +9,11 @@ from interleave_epub.flask_app.asset_loader import pipe_loader
 def index():
     """Render the index page."""
     pipe_loader()
-    tran = gs["pipeline"]("Does the pipeline work for translation tasks?")
+    print(gs)
+
+    lt = "en"
+    lt_other = "fr"
+    pipe_key = f"pipe_{lt}_{lt_other}"
+    tran = gs[pipe_key]("Does the pipeline work for translation tasks?")
     print(f"translation {tran}")
     return render_template("index.html", title="Home")
