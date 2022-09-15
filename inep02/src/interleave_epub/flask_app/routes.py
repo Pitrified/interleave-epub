aae94accf32bdebd1d5f49bc184dfa5272bcc025
Pitrified <nobilipietro@gmail.com>
2022-09-15 23:29:40 +0200

    Add minimal dropdown form

--- inep02/src/interleave_epub/flask_app/routes.py
+++ inep02/src/interleave_epub/flask_app/routes.py
@@ -1,6 +1,6 @@
 """Routes for the flask app."""
 
-from flask import render_template
+from flask import render_template, request
 
 from interleave_epub.flask_app import app
 
@@ -10,3 +10,23 @@ from interleave_epub.flask_app import app
 def index():
     """Render the index page."""
     return render_template("index.html", title="Home")
+
+
+@app.route("/load", methods=["GET", "POST"])
+def load_ep():
+    """Render the page to pick the languages and load the epubs."""
+    # parse POST request
+    if request.method == "POST":
+        print(f"{request=}")
+        print(f"{request.form=}")
+        print(f"{request.files=}")
+        # request.form['manu']
+
+    lts_list = ["en", "fr", "it"]
+    lt_src_sel = "fr"
+
+    return render_template(
+        "load.html",
+        lts_list=lts_list,
+        lt_src_sel=lt_src_sel,
+    )
